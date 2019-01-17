8a7d4125744a2d7296aa9e596b0eeacdc93c41df
JackYang3567 <13808013567@163.com>
2019-01-17 15:23:34 +0800

    first commit

---
+++ Models/StudentDbContext.cs
@@ -0,0 +1,9 @@
+﻿using System.Data.Entity;
+
+namespace AspNetMvc.QuickStart.Models
+{
+    public class StudentDbContext : DbContext
+    {
+        public System.Data.Entity.DbSet<AspNetMvc.QuickStart.Models.Student> Students { get; set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+"# MVC-for-asp.net" 
+"# http://www.cnblogs.com/sanshi/p/6210695.html" 
